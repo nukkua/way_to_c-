c18e9b6bc53fa7b2ececacc71ad0996c8c85a9d2
nukkua <marco.salazar.2014.com@gmail.com>
2024-01-02 18:28:22 -0400

    day 2

--- vectors/index.cpp
+++ vectors/index.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <vector>
 
+#define forIt(a,b) for(int (a):(b))
+
+typedef std::vector<int> vInt;
+
 int main() {
   std::vector<int> v{2, 5, 6, 1, 1};
 
-  std::vector<int> v1(10, 10);
+  vInt v1(10);
   // El primer parametro es el size del vector, el segundo parametro es con que
-  // mierda voy a rellenar
+  // mierda voy a rellenar, y sino lo especifico llena con 0
 
   for (int i = 0; i < v.size(); ++i) {
     std::cout << v[i] << "\n";
+  }
+  forIt(i, v1){
+    std::cout << v1[i] << "\n";
   }
 
   std::cout << v.front() << "first element of the vector\n";
@@ -29,5 +36,4 @@ int main() {
 	  std::cout << v1[i] << "\n";
   }
   v1.clear(); // limpia totalmente el vector como si fuera el poto de chesco
-	
 }
